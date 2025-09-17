f4f6b82dff5a8aab76bc4d58cffa0c24c8d06784
rohit3171999 <rohit3171999@gmail.com>
2025-09-17 14:58:21 +0530

    add functional component in HeroSection.jsx

---
+++ src/components/HeroSection.jsx
@@ -0,0 +1,21 @@
+/*
+* Description: The top part of the landing page. Immediately captures attention with a headline, subheadline, and signup form.
+Purpose: Encourage users to subscribe right away.
+* */
+export default function HeroSection() {
+    return (
+
+        // <section> → wrapper for hero
+        <section>
+            {/*<h1> → main headline (“Get Smarter Every Week”)*/}
+            <h1></h1>
+            {/*<p> → subheadline pitch (“Join thousands of readers receiving weekly insights on tech and productivity”)*/}
+            <p></p>
+            {/*<form> → email signup form*/}
+            <form action=""></form>
+            {/*<input type="email"> → email field with validation*/}
+            {/*<button> → Subscribe CTA*/}
+        </section>
+
+    );
+};
