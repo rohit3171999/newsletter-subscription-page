92b2d2811be7a3f92b674534ffb3f6c02f027baa
rohit3171999 <rohit3171999@gmail.com>
2025-09-17 15:05:57 +0530

    add functional component in PrivacyNote.jsx

---
+++ src/components/PrivacyNote.jsx
@@ -0,0 +1,13 @@
+/*
+* Description: Small text reassuring users about privacy.
+Purpose: Build trust and reduce hesitation in submitting email.
+* */
+export default function PrivacyNote(){
+    return (
+        <>
+            {/*<section> or <div> → wrapper*/}
+
+            {/*    <p> → privacy text (“We respect your privacy. No spam, unsubscribe anytime.”)*/}
+        </>
+    );
+}
