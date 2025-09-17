bfc4e3909ffb883557c32afad52918b1b8138376
rohit3171999 <rohit3171999@gmail.com>
2025-09-17 15:09:13 +0530

    add functional component in Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,27 @@
+/*
+* Description: Bottom section with legal links and social icons.
+Purpose: Provide credibility, navigation, and compliance information.
+* */
+export default function Footer(){
+    return (
+        <>
+            {/*<footer> → wrapper*/}
+
+            {/*    <nav> → navigation for footer links*/}
+
+            {/*        <ul>*/}
+
+            {/*            <li><a> → About link*/}
+
+            {/*                <li><a> → Contact link*/}
+
+            {/*                    <li><a> → Privacy Policy link*/}
+
+            {/*                        <li><a> → Terms link*/}
+
+            {/*                            <div> (optional) → social media icons*/}
+
+            {/*                                <a> with <svg> or <img> → social icon links*/}
+        </>
+    );
+}
