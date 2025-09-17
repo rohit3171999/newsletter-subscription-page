a0256ac8715b9e9ba3fa9e2d020e699e6310d10f
rohit3171999 <rohit3171999@gmail.com>
2025-09-17 15:02:48 +0530

    add functional component in ValuePropositionSection.jsx

---
+++ src/components/ValuePropositionSection.jsx
@@ -0,0 +1,21 @@
+/*
+* Description: Highlights the key benefits of subscribing.
+Purpose: Reinforce value and increase conversions.
+* */
+export default function ValuePropositionSection(){
+    return (
+        <>
+            {/*<section> → wrapper*/}
+
+            {/*    <h2> → section heading (Why Subscribe?)*/}
+
+            {/*        <ul> → unordered list*/}
+
+            {/*            <li> → benefit #1 (Curated content)*/}
+
+            {/*                <li> → benefit #2 (Actionable insights)*/}
+
+            {/*                    <li> → benefit #3 (Free forever)*/}
+        </>
+    );
+}
